d0480b0fc42674f69d7bc10b8316df894120e4f6
vagner santos <vagner131417@gmail.com>
2023-02-05 13:52:21 -0300

    dash update

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,79 +1,81 @@
-import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
-import dynamic from "next/dynamic";
-import { Header } from "../components/Header";
-import { Sidebar } from "../components/Sidebar";
+import { Box, Flex, SimpleGrid, Text, theme } from '@chakra-ui/react';
+import { ApexOptions } from 'apexcharts';
+import dynamic from 'next/dynamic';
 
-const Chart = dynamic(() => import("react-apexcharts"), {
-  ssr: false,
-});
+import { Header } from '../components/Header';
+import { Sidebar } from '../components/Sidebar';
 
-const options = {
+const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
+
+const options: ApexOptions = {
   chart: {
-    toolbar: {
-      show: false,
-    },
-    zoom: {
-      enabled: false,
-    },
-    foreColor: theme.colors.gray[500],
+    toolbar: { show: false },
+    zoom: { enabled: false },
+    foreColor: theme.colors.gray['500'],
   },
-  grid: {
-    show: false,
-  },
-  dataLabels: {
-    enabled: false,
-  },
-  tooltip: {
-    enabled: false,
-  },
-
+  grid: { show: false },
+  dataLabels: { enabled: false },
+  tooltip: { enabled: false },
   xaxis: {
-    type: "datetime",
-    axisBorder: {
-      color: theme.colors.gray[600],
-    },
-    axisTicks: {
-      color: theme.colors.gray[600],
-    },
+    type: 'datetime',
+    axisBorder: { color: theme.colors.gray['600'] },
+    axisTicks: { color: theme.colors.gray['600'] },
     categories: [
-      "2023-03-18T00:00:000",
-      "2023-03-19T00:00:000",
-      "2023-03-20T00:00:000",
-      "2023-03-21T00:00:000",
-      "2023-03-22T00:00:000",
-      "2023-03-23T00:00:000",
-      "2023-03-24T00:00:000",
+      '2021-04-02T00:00:00:00.000Z',
+      '2021-04-03T00:00:00:00.000Z',
+      '2021-04-04T00:00:00:00.000Z',
+      '2021-04-05T00:00:00:00.000Z',
+      '2021-04-06T00:00:00:00.000Z',
+      '2021-04-07T00:00:00:00.000Z',
     ],
   },
   fill: {
     opacity: 0.3,
-    type: "gradient",
+    type: 'gradient',
     gradient: {
-      shade: "dark",
+      shade: 'dark',
       opacityFrom: 0.7,
       opacityTo: 0.3,
     },
   },
 };
 
-const series = [{ name: "series1", data: [31, 120, 10, 28, 61, 18, 109] }];
+const series = [{ name: 'series1', data: [57, 36, 13, 29, 6, 19] }];
 
 export default function Dashboard() {
   return (
-    <Flex direction="column" h="100vh">
+    <Flex direction="column" height="100vh">
       <Header />
 
-      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+      <Flex
+        width="100%"
+        maxWidth={1480}
+        marginY="6"
+        marginX="auto"
+        paddingX="6"
+      >
         <Sidebar />
-        <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
-          <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
-            <Text fontSize="lg" mb="4">
-              Incritos da semana
+
+        <SimpleGrid flex="1" gap="4" minChildWidth="320px" alignContent="flex-start">
+          <Box
+            padding={['6', '8']}
+            bg="gray.800"
+            borderRadius={8}
+            paddingBottom="4"
+          >
+            <Text fontSize="lg" marginBottom="4">
+              Inscritos da semana
             </Text>
             <Chart options={options} series={series} type="area" height={160} />
           </Box>
-          <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
-            <Text fontSize="lg" mb="4">
+
+          <Box
+            padding={['6', '8']}
+            bg="gray.800"
+            borderRadius={8}
+            paddingBottom="4"
+          >
+            <Text fontSize="lg" marginBottom="4">
               Taxa de abertura
             </Text>
             <Chart options={options} series={series} type="area" height={160} />
