0b82ce8d97a52e3702cebbf41f1fcefe5b021c7e
vagner santos <vagner131417@gmail.com>
2023-02-14 15:17:49 -0300

    chore: add queryclient

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { ReactQueryDevtools } from 'react-query/devtools'
 import { theme } from "../styles/theme";
 
-import { QueryClientProvider, QueryClient } from 'react-query';
+import { QueryClientProvider } from 'react-query';
 import { SidebarDrawerProvider } from "../contexts/SidebarDrawerContext";
 import { makeServer } from "../services/mirage";
 import { queryClient } from "../services/queryClient";
